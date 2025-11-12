b926e0c266ba127fbdba3721f0e0da7965f1c30a
Evangelos Kolimitras <evangelos.kolimitras@conceptboard.com>
2025-11-12 15:18:33 +0100

    update package

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,6 @@
 // This is the core state management
-export { signal } from "./reactivity";
-export { FlickElement, FlickSvgElement } from "./core";
+export { signal } from "@flick/comms/";
+export { FlickElement, FlickSvgElement } from "@flick/comms/";
 
 // NOTE: We do NOT export src/main.ts or src/worker-api.ts.
 // Those are the *runtime* and are not part of the
