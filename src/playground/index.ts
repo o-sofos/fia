452119388496602b5201729284232b131f0599a6
Evangelos <evangelos.kolimitras@outlook.com>
2026-01-12 19:20:20 +0100

    update playground

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,17 +1,4 @@
-import { $, button, div } from "../core/mod";
-
+//Playground
 export default (() => {
-
-  const style = $<{ color: "red" | "blue" }>({
-    color: "red",
-  });
-
-  button('update color', {
-    onclick: () => {
-      style.value = { ...style.value, color: style.value.color === "red" ? "blue" : "red" };
-    }
-  });
-
-  div("hey bro", { style });
 
 })();
