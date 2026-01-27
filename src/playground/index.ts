17e4c3270acbb77d7638e9af36314f99a4e2ccd9
Evangelos <evangelos.kolimitras@outlook.com>
2026-01-27 20:10:39 +0100

    update playground

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,9 +1,18 @@
-import { div } from "../core/mod";
+import { $, button, h1, h2, h3, h4, h5, h6, p } from "../core/mod";
 
 export default (() => {
-  div("Correct ARIA", {
-    role: "alert",
-    ariaLive: "assertive",
-    ariaHidden: true
-  });
+  const count = $(0);
+
+  h1(count);
+  h2(count);
+  h3(count);
+  h4(count);
+  h5(count);
+  h6(count);
+
+  p($(() => "count: " + count.value));
+
+  const onclick = () => count.value++
+
+  button("increment", { onclick });
 })();
